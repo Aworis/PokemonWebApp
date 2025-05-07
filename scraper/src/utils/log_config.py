82124da3ac1635d5770708ba2fd4e17bbb386b71
Benjamin Pokrzywa <98288484+Aworis@users.noreply.github.com>
2025-05-07 21:48:09 +0200

    Logging Konfiguration implementiert

---
+++ scraper/src/utils/log_config.py
@@ -0,0 +1,16 @@
+import logging
+from pathlib import Path
+
+def setup_logging(log_file: str = "logs/app.log") -> None:
+    log_path = Path(log_file)
+    log_path.parent.mkdir(parents=True, exist_ok=True)  # Ordner erstellen, falls nicht da
+
+    logging.basicConfig(
+        level=logging.INFO, # Niedrigstes Log-Level
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        handlers=[
+            #TODO: Loggin in app.log fnuktioniert noch nicht
+            logging.FileHandler(log_path, encoding="utf-8"),
+            logging.StreamHandler()
+        ]
+    )
