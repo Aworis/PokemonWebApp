9066d565cf57bea4bd50e6a2d8f3c10c39a83cd3
Benjamin Pokrzywa <98288484+Aworis@users.noreply.github.com>
2025-11-01 21:28:22 +0100

    Kommentare in main.py ergänzt

--- scraper/src/main.py
+++ scraper/src/main.py
@@ -6,13 +6,15 @@ from utils.logging_config import setup_logging
 # TODO: logging ordner ändern auf scraping logger
 setup_logging()
 
-# Initialisierung der Komponenten
+# Initialisierung der zentralen Scraper-Komponente
 scraper_manager = ScraperManager()
 session = scraper_manager.session
 
+# Laden der Sitemap-Konfiguration aus YAML-Datei
 config_loader = ConfigLoader()
 sitemaps = config_loader.load_sitemap_urls()
 
+# Extrahieren aller URLs aus den jeweiligen Sitemaps
 for key in sitemaps:
     sitemap_url = sitemaps.get(f"{key}")
 
@@ -27,4 +29,5 @@ for key in sitemaps:
 
     scraper_manager.register_scraper(key, urls)
 
+# Ausführung aller registrierten Scraper
 scraper_manager.run_all()
