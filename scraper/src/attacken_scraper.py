42bf97dd59d5fdf54328b2979acedeeb189ff35c
Benjamin Pokrzywa <98288484+Aworis@users.noreply.github.com>
2025-11-29 17:21:14 +0100

    Docstring ergänzt gemäß ER-Modell

--- scraper/src/attacken_scraper.py
+++ scraper/src/attacken_scraper.py
@@ -7,6 +7,21 @@ from abstract_web_scraper import WebScraper
 logger = logging.getLogger(__name__)
 
 class AttackenScraper(WebScraper):
+    """
+    Webscraper für Pokémon-Attacken.
+
+    Dieser Scraper ruft HTML-Seiten ab, die Informationen zu Pokémon-Attacken
+    enthalten, und extrahiert aus dem HTML die relevanten Daten für
+    den Pokédex.
+
+    Extrahierte Daten:
+        - name: Der Name der Attacke.
+        - beschreibung: Eine kurze textuelle Beschreibung der Attacke.
+        - effekt:
+        - staerke:
+        - genauigkeit:
+        - angriffspunkte:
+    """
 
     def _extract_data(self, soup: BeautifulSoup) -> list[dict]:
         block = soup.select_one(".well")
@@ -15,6 +30,6 @@ class AttackenScraper(WebScraper):
 
         data = {
             "name": block.select_one("h1").get_text(strip=True),
-            "beschreibung": block.select_one("p").get_text(strip=True)
+            "beschreibung": block.select_one("p").get_text(strip=True),
         }
         return [data]
